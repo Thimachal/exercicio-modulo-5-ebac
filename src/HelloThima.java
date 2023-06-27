cde08eeeef27edab80a3fc348e8b1a0319b3f793
Thimachal <thimachal@gmail.com>
2023-06-27 20:30:06 -0300

    push do primeiro exercicio

---
+++ src/HelloThima.java
@@ -0,0 +1,12 @@
+public class HelloThima{
+    public static void main(String[] args) {
+        System.out.println("Hi my fisrt print in Java, i'm Thiago Oliveira");
+
+        int number1 = 1;
+        int number2 = 9;
+        int soma=0;
+
+        soma=number1+number2;
+        System.out.println("My note in the end course: " + soma);
+    }
+}
